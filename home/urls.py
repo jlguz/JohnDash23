aa0955b4fbf5cb79cb13609ef91dcb7cfdce5a8a
JohnGuzman <jlg05@hotmail.co.uk>
2023-10-21 14:01:57 +0100

    2commit

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.home, name=''),
-
+    path('index', views.home, name='index'),
+    path('register', views.register, name='register'),
+    path('', views.my_login, name=''),
+    path('user-logout', views.user_logout, name='user-logout'),
 ]
 
--- home/views.py
+++ home/views.py
@@ -1,4 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import CreateUserForm, LoginForm
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 def home(request):
-    return render(request, 'home/index.html')
+      return render(request, 'home/index.html')
+
+#Regeister a user
+
+def register(request):
+      
+      form = CreateUserForm()
+
+      if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                  form.save()
+
+                  return redirect('my-login')
+
+      context = {'form':form}
+
+      return render(request, 'home/register.html', context=context)
+
+
+def my_login(request):
+      form = LoginForm()
+
+      if request.method == 'POST':
+            form = LoginForm(request, data=request.POST)
+
+            if form.is_valid():
+                  
+                  username = request.POST.get('username')
+                  password = request.POST.get('password')
+
+                  user = authenticate(request, username=username, password=password)
+                  
+                  if user is not None:
+                        auth.login(request, user)
+                        
+                        return redirect('index')
+
+      context = {'form':form}
+      return render(request, 'home/my-login.html', context=context)
+
+
+# user logout
+
+def user_logout(request):
+
+      auth.logout(request)
+
+      return redirect('my-login')  
